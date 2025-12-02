6ce4f705bfeb8e09ba189f047470126532fe294e
Laura <92202491+cly163519@users.noreply.github.com>
2025-12-02 16:44:15 +1300

    Update AuditStatusEnum messages

--- tool_backend/src/main/java/cn/tedu/tool_backend/base/enums/AuditStatusEnum.java
+++ tool_backend/src/main/java/cn/tedu/tool_backend/base/enums/AuditStatusEnum.java
@@ -6,10 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AuditStatusEnum {
-    MY_PENDING("10","待我审核"),
-    PENDING("20","待他人审核"),
-    AUDITED("30","已审核"),
-    REJECT("40","驳回");
+    MY_PENDING("10","Needs to be reviewed by me."),
+    PENDING("20","Needs to be reviewed by others."),
+    AUDITED("30","Reviewed"),
+    REJECT("40","Rejection");
 
     private String code;
     private String msg;
