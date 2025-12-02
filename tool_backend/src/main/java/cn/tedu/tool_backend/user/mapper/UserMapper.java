a0e343366d3a4e03a82c50d4e74f1c7d97c5120b
Laura <92202491+cly163519@users.noreply.github.com>
2025-12-02 17:30:36 +1300

    Remove comment
    Removed comment explaining the use of User object for update operations.

--- tool_backend/src/main/java/cn/tedu/tool_backend/user/mapper/UserMapper.java
+++ tool_backend/src/main/java/cn/tedu/tool_backend/user/mapper/UserMapper.java
@@ -12,12 +12,7 @@ public interface UserMapper {
     UserVO selectByUsername(String username);
     void insert(User user);
     List<UserVO> selectUser(UserQuery userQuery);
-    /*
-        下面使用 User 对象作为参数，
-        是因为这个方法需要接收一个包含完整用户信息的对象，
-        以便进行更新操作。而 UserQuery 对象通常用于查询操作，
-        不适合作为更新方法的参数。
-     */
+  
     void update(User user);
 
     void deleteById(Long userId);
