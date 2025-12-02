ad6bbe9b367ca18234b44e43ad99d08ab44858c6
Laura <92202491+cly163519@users.noreply.github.com>
2025-12-02 17:31:25 +1300

    Refactor UserService

--- tool_backend/src/main/java/cn/tedu/tool_backend/user/service/UserService.java
+++ tool_backend/src/main/java/cn/tedu/tool_backend/user/service/UserService.java
@@ -7,10 +7,8 @@ import cn.tedu.tool_backend.user.pojo.vo.UserVO;
 
 import java.util.List;
 
-public interface UserService {//UserService 接口声明了一个方法 login，
-                            // 这个方法接受一个 UserLoginParam 对象作为参数，并返回一个 UserVO 对象。
-    UserVO login(UserLoginParam userLoginParam);//UserLoginParam 可能包含用户登录所需的参数（如用户名和密码）。
-                                                //UserVO 可能是用户登录成功后返回的用户信息对象。
+public interface UserService {
+    UserVO login(UserLoginParam userLoginParam);
     void saveUser(UserSaveParam userSaveParam);
     List<UserVO> selectUser(UserQuery userQuery);
     void resetPassword(Long userId);
