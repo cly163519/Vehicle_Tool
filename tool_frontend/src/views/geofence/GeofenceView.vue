091e27a5d69f3cdd9b39afd1e38aa0495f7652e4
Laura <92202491+cly163519@users.noreply.github.com>
2025-12-03 16:59:47 +1300

    Update GeofenceView

--- tool_frontend/src/views/geofence/GeofenceView.vue
+++ tool_frontend/src/views/geofence/GeofenceView.vue
@@ -1,75 +1,75 @@
-<!--电子围栏页-->
+<!-- Electric geoFence page -->
 <template>
   <div style="height: 6vh;background-color:#fff;padding:10px 20px;">
-    <span style="line-height:60px;font-size:20px;">围栏管理</span>
+    <span style="line-height:60px;font-size:20px;">Fence management</span>
     <el-button type="primary" style="float:right;margin-top:13px;"
-    @click="router.push('/geofenceMap')">新建围栏</el-button>
+    @click="router.push('/geofenceMap')">Add new fence</el-button>
   </div>
-<!--围栏查询卡片-->
+<!-- Fence searching card -->
 <el-card style="margin:20px;height:70px;">
   <el-form :inline="true">
-    <el-form-item label="围栏名称">
-      <el-input placeholder="请输入" style="width:220px;"
+    <el-form-item label="Fence name">
+      <el-input placeholder="Please enter" style="width:220px;"
       v-model="geoSearchForm.name" @keydown.enter="loadGeo"></el-input>
     </el-form-item>
-    <el-form-item label="围栏状态" style="width:290px;">
-      <el-select placeholder="请选择" v-model="geoSearchForm.status" @change="loadGeo">
-        <el-option label="启用" value="1"></el-option>
-        <el-option label="禁用" value="0"></el-option>
+    <el-form-item label="Fence status" style="width:290px;">
+      <el-select placeholder="Please select" v-model="geoSearchForm.status" @change="loadGeo">
+        <el-option label="Enable" value="1"></el-option>
+        <el-option label="Disable" value="0"></el-option>
       </el-select>
     </el-form-item>
     <el-form-item>
-      <el-button @click="resetSearch">重置</el-button>
-      <el-button type="primary" @click="loadGeo">查询</el-button>
+      <el-button @click="resetSearch">Reset</el-button>
+      <el-button type="primary" @click="loadGeo">Search</el-button>
     </el-form-item>
   </el-form>
 </el-card>
-<!--围栏表格数据-->
+<!--Fence tabel data-->
 <el-card style="margin:20px;">
   <el-table stripe :data="geoTableData">
-    <el-table-column label="编号" prop="id" type="index" align="center" width="100"></el-table-column>
-    <el-table-column label="围栏名称" prop="name" align="center"></el-table-column>
-    <el-table-column label="围栏坐标点" prop="position" align="center" show-overflow-tooltip="true" width="500"></el-table-column>
-    <el-table-column label="创建时间" prop="createTime" align="center"></el-table-column>
-    <el-table-column label="围栏状态" align="center">
+    <el-table-column label="Number" prop="id" type="index" align="center" width="100"></el-table-column>
+    <el-table-column label="Fence name" prop="name" align="center"></el-table-column>
+    <el-table-column label="Fence coordinate" prop="position" align="center" show-overflow-tooltip="true" width="500"></el-table-column>
+    <el-table-column label="Creation time" prop="createTime" align="center"></el-table-column>
+    <el-table-column label="Fence status" align="center">
       <template #default="scope">
         <el-switch inactive-value="0" active-value="1" v-model="scope.row.status"
         @change="changeStatus(scope.row.id,scope.row.status)"></el-switch>
       </template>
     </el-table-column>
-    <el-table-column label="操作" align="center">
+    <el-table-column label="Operation" align="center">
       <template #default="scope">
-        <el-button type="primary" link @click="loadVehicle(scope.row.id)">管理车辆</el-button>
-        <el-button type="primary" link @click="deleteGeo(scope.row.id)">删除</el-button>
+        <el-button type="primary" link @click="loadVehicle(scope.row.id)">Manage vehicle</el-button>
+        <el-button type="primary" link @click="deleteGeo(scope.row.id)">Delete</el-button>
       </template>
     </el-table-column>
   </el-table>
 </el-card>
-<!--已绑定车辆弹窗-->
-<el-dialog title="已绑定车辆列表" v-model="bindVehicleDialogVisible" style="padding:40px;">
-  <el-button type="primary" style="margin:10px 0;" @click="showUnbindVehicleList">添加绑定车辆</el-button>
+<!-- Vehicle bounded pop-up window -->
+<el-dialog title="Bounded vehicle list" v-model="bindVehicleDialogVisible" style="padding:40px;">
+  <el-button type="primary" style="margin:10px 0;" @click="showUnbindVehicleList">Add bounding vehicle</el-button>
     <el-table :data="bindVehicleArr">
-      <el-table-column prop="brand" label="车辆品牌" align="center"></el-table-column>
-      <el-table-column prop="license" label="车牌号" align="center"></el-table-column>
-      <el-table-column prop="type" label="车辆类型" align="center"></el-table-column>
-      <el-table-column label="操作" fixed="right" width="200" align="center">
+      <el-table-column prop="brand" label="Vehicle brand" align="center"></el-table-column>
+      <el-table-column prop="license" label="Vehicle number" align="center"></el-table-column>
+      <el-table-column prop="type" label="Vehicle type" align="center"></el-table-column>
+      <el-table-column label="operation" fixed="right" width="200" align="center">
         <template #default="scope">
-          <el-button link type="primary" @click="removeBindVehicle(scope.row.id)">移除</el-button>
+          <el-button link type="primary" @click="removeBindVehicle(scope.row.id)">Remove</el-button>
         </template>
       </el-table-column>
     </el-table>
 </el-dialog>
-<!--添加可绑定车辆列表弹窗 -->
-<el-dialog title="可绑定车辆列表" v-model="AddBindVehicleDialogVisible" :before-close="handleClose">
+<!--Add a pop-up window to the list of vehicles that can be bound -->
+<el-dialog title="List of vehicles that can be bound" v-model="AddBindVehicleDialogVisible" :before-close="handleClose">
   <el-table :data="unBindVehicleArr">
-    <el-table-column label="编号" type="index" align="center" width="80px;"></el-table-column>
-    <el-table-column prop="brand" label="车辆品牌" align="center"></el-table-column>
-    <el-table-column prop="license" label="车牌号" align="center"></el-table-column>
-    <el-table-column prop="type" label="车辆类型" align="center"></el-table-column>
-    <el-table-column prop="status" label="车辆状态" align="center"></el-table-column>
-    <el-table-column label="操作" align="center">
+    <el-table-column label="Code" type="index" align="center" width="80px;"></el-table-column>
+    <el-table-column prop="brand" label="Vehicle brand" align="center"></el-table-column>
+    <el-table-column prop="license" label="License number" align="center"></el-table-column>
+    <el-table-column prop="type" label="Vehicle type" align="center"></el-table-column>
+    <el-table-column prop="status" label="Vehicle status" align="center"></el-table-column>
+    <el-table-column label="operations" align="center">
       <template #default="scope">
-        <el-button link type="primary" @click="bindNewVehicle(scope.row.id)">绑定</el-button>
+        <el-button link type="primary" @click="bindNewVehicle(scope.row.id)">Bound</el-button>
       </template>
     </el-table-column>
   </el-table>
@@ -83,21 +83,16 @@ import {ElMessage} from "element-plus";
 import axios from "axios";
 import qs from "qs";
 
-//已绑定车辆列表弹窗
+//List of bound vehicles pop-up window
 const bindVehicleDialogVisible = ref(false);
-//可绑定车辆列表弹窗
+//Available vehicle list pop-up
 const AddBindVehicleDialogVisible = ref(false);
 
 const geoSearchForm = ref({name:'', status:''})
 const geoTableData = ref([]);
 
 const loadGeo = () =>{
-  /*
-  作用是将 geoSearchForm.value 中包含的查询条件对象序列化为一个 URL 查询字符串。
-  这个字符串可以附加在 URL 的末尾，用于发送带有查询参数的 GET 请求。
-  比如，如果 geoSearchForm.value 的内容是 { name: 'test', status: 'active' }，
-  那么 data 的值将是 'name=test&status=active'。
-   */
+
   let data = qs.stringify(geoSearchForm.value);
   axios.get(BASE_URL+'/v1/geofence/select?'+data).then((response)=>{
     if(response.data.code === 2000){
@@ -132,7 +127,7 @@ const loadVehicle = (geofenceId)=>{
 const removeBindVehicle = (id) =>{
   axios.post(BASE_URL+'/v1/vehicle/unbind/'+id).then((response)=>{
     if(response.data.code === 2000){
-      ElMessage.success('车辆解绑成功');
+      ElMessage.success('Vehicle unbinding successful');
       loadVehicle(geoId);
     }else{
       ElMessage.error(response.data.msg);
@@ -153,7 +148,7 @@ const showUnbindVehicleList = () =>{
 const bindNewVehicle = (id)=>{
   axios.post(BASE_URL+'/v1/vehicle/bind/'+geoId+'/'+id).then((response)=>{
     if(response.data.code === 2000){
-      ElMessage.success('车辆绑定成功');
+      ElMessage.success('Vehicle binding successful');
       showUnbindVehicleList();
     }else{
       ElMessage.error(response.data.msg);
@@ -168,19 +163,19 @@ const changeStatus = (id,status)=>{
   let data = qs.stringify({id:id,status:status});
   axios.post(BASE_URL+'/v1/geofence/save',data).then((response)=>{
     if(response.data.code ===2000){
-      ElMessage.success('修改状态成功');
+      ElMessage.success('Status changed successfully');
     }else{
       ElMessage.error(response.data.msg);
     }
   })
 }
 const deleteGeo = (id) =>{
-  if(confirm('是否删除该围栏?')){
+  if(confirm('Should the fence be deleted??')){
     axios.post(BASE_URL+'/v1/geofence/delete/'+id).then((response)=>{
       if(response.data.code === 2000){
-        ElMessage.success('删除围栏成功');
+        ElMessage.success('Fence removed successfully');
       }else{
-        ElMessage.error('请先移除已绑定车辆');
+        ElMessage.error('Please remove the bound vehicle first.');
       }
     })
   }
