3a2e9f9bd521cd35579f9161ca92123975a10b34
Laura <92202491+cly163519@users.noreply.github.com>
2025-12-06 20:08:08 +1300

    Update DistributeView

--- tool_frontend/src/views/schedule/DistributeView.vue
+++ tool_frontend/src/views/schedule/DistributeView.vue
@@ -1,62 +1,62 @@
 <!--Vehicle allocation management page-->
 <template>
   <div style="height: 6vh;background-color:#fff;padding:10px 20px;">
-    <span style="line-height:60px;font-size:20px;">车辆分配</span>
+    <span style="line-height:60px;font-size:20px;">Vehicle allocation</span>
   </div>
-  <!-- 申请单搜索卡片 -->
+  <!-- Application form search card -->
   <el-card style="margin:20px;height: 70px;">
     <el-form :inline="true">
-      <el-form-item label="工单申请人">
-        <el-input placeholder="请输入申请人" style="width:220px;"
+      <el-form-item label="Applicant">
+        <el-input placeholder="Please enter applicant" style="width:220px;"
                   v-model="search.username" @keydown.enter.prevent="loadApplication"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button @click="resetSearch">重置</el-button>
-        <el-button type="primary" @click="loadApplication">查询</el-button>
+        <el-button @click="resetSearch">Reset</el-button>
+        <el-button type="primary" @click="loadApplication">Research</el-button>
       </el-form-item>
     </el-form>
   </el-card>
 
-  <!-- 工单信息列表 -->
+  <!-- Application form list -->
   <el-card  style="margin:20px;">
     <el-radio-group style="margin-bottom: 15px;" v-model="type">
-      <el-radio-button value="50" size="large" @change="loadApplication">待分配</el-radio-button>
-      <el-radio-button value="60" size="large" @change="loadApplication">已分配</el-radio-button>
+      <el-radio-button value="50" size="large" @change="loadApplication">Pending allocation</el-radio-button>
+      <el-radio-button value="60" size="large" @change="loadApplication">Allocated</el-radio-button>
     </el-radio-group>
     <el-table :data="appTableData">
-      <el-table-column label="编号" type="index" width="100" align="center"></el-table-column>
-      <el-table-column label="工单申请人" prop="username" align="center"></el-table-column>
-      <el-table-column label="申请时间" prop="createTime" align="center"></el-table-column>
-      <el-table-column label="出发地" prop="departureAddr" align="center"></el-table-column>
-      <el-table-column label="目的地" prop="destinationAddr" align="center"></el-table-column>
-      <el-table-column label="用车事由" prop="reason" align="center"></el-table-column>
-      <el-table-column label="使用开始时间" prop="startTime" align="center"></el-table-column>
-      <el-table-column label="使用结束时间" prop="endTime" align="center"></el-table-column>
-      <el-table-column label="操作" width="100" align="center">
+      <el-table-column label="Number" type="index" width="100" align="center"></el-table-column>
+      <el-table-column label="Applicant" prop="username" align="center"></el-table-column>
+      <el-table-column label="Application time" prop="createTime" align="center"></el-table-column>
+      <el-table-column label="Departure" prop="departureAddr" align="center"></el-table-column>
+      <el-table-column label="Destination" prop="destinationAddr" align="center"></el-table-column>
+      <el-table-column label="Reason for vehicle use" prop="reason" align="center"></el-table-column>
+      <el-table-column label="Start time" prop="startTime" align="center"></el-table-column>
+      <el-table-column label="End time" prop="endTime" align="center"></el-table-column>
+      <el-table-column label="Operation" width="100" align="center">
         <template #default="scope">
-          <!--只有"已审核"才显示分配用车按钮  只有"分配用车"才显示还车按钮-->
-          <el-button type="primary" link v-if="type==50" @click="loadGeo(scope.row.id)">分配用车</el-button>
-          <!--注意:我们现在在申请表表格中,这里的scope.row表示当前申请单的完整对象,既包含申请单id,也包含车辆id-->
-          <el-button type="primary" link v-if="type==60" @click="back(scope.row)">还车</el-button>
+          <!--The "Assign a vehicle" button is only displayed when the application is "Approved". The "Return a vehicle" button is only displayed when a vehicle has been assigned-->
+          <el-button type="primary" link v-if="type==50" @click="loadGeo(scope.row.id)">Vehicle allocation</el-button>
+          <!--Note: In the application form, scope.row represents the complete object of the current application, including both the application ID and the vehicle ID-->
+          <el-button type="primary" link v-if="type==60" @click="back(scope.row)">Return vehicle</el-button>
         </template>
       </el-table-column>
     </el-table>
   </el-card>
 
-  <!-- 围栏车辆概览弹窗 -->
-  <el-dialog title="围栏列表" v-model="dialogVisible" style="padding:40px;" :before-close="handleClose">
-    <el-form>
+  <!-- Fence vehicle overview pop-up -->
+  <el-dialog title="Fence list" v-model="dialogVisible" style="padding:40px;" :before-close="handleClose">
+    <el-form>Fence vehicle overview pop-up
       <el-row :gutter="10">
         <el-col :span="8">
-          <el-form-item label="围栏名称:">
-            <el-input placeholder="请输入" v-model="searchGeofenceForm.name" @keydown.enter="loadGeo"></el-input>
+          <el-form-item label="Fence name: ">
+            <el-input placeholder="Please enter" v-model="searchGeofenceForm.name" @keydown.enter="loadGeo"></el-input>
           </el-form-item>
         </el-col>
         <el-col :span="8">
-          <el-form-item label="围栏状态">
-            <el-select placeholder="请选择" v-model="searchGeofenceForm.status" @change="loadGeo">
-              <el-option label="启用" value="1"/>
-              <el-option label="禁用" value="0"/>
+          <el-form-item label="Fence status">
+            <el-select placeholder="Please choice" v-model="searchGeofenceForm.status" @change="loadGeo">
+              <el-option label="Enable" value="1"/>
+              <el-option label="Disable" value="0"/>
             </el-select>
           </el-form-item>
         </el-col>
@@ -64,38 +64,38 @@
         <el-col :span="4">
           <el-form-item>
             <el-button-group>
-              <el-button @click="resetSearchGeo">重置</el-button>
-              <el-button type="primary" @click="loadGeo">查询</el-button>
+              <el-button @click="resetSearchGeo">Reset</el-button>
+              <el-button type="primary" @click="loadGeo">Search</el-button>
             </el-button-group>
           </el-form-item>
         </el-col>
       </el-row>
     </el-form>
     <el-table style="margin:20px 0" :data="geoTableData">
-      <el-table-column label="编号" type="index" width="80" align="center"></el-table-column>
-      <el-table-column label="围栏名称" prop="name" width="200" align="center"></el-table-column>
-      <el-table-column label="车辆总数" prop="totalNum" width="200" align="center"></el-table-column>
-      <el-table-column label="可用车辆" prop="availableNum" align="center"></el-table-column>
-      <el-table-column label="围栏状态" prop="status" align="center" :formatter="geofenceStatusFormatter"></el-table-column>
-      <el-table-column label="操作" width="100" align="center">
+      <el-table-column label="Number" type="index" width="80" align="center"></el-table-column>
+      <el-table-column label="Fence name" prop="name" width="200" align="center"></el-table-column>
+      <el-table-column label="Total vehicle" prop="totalNum" width="200" align="center"></el-table-column>
+      <el-table-column label="Available vehicle" prop="availableNum" align="center"></el-table-column>
+      <el-table-column label="Fence status" prop="status" align="center" :formatter="geofenceStatusFormatter"></el-table-column>
+      <el-table-column label="Operation" width="100" align="center">
         <template #default="scope">
-          <el-button type="primary" link @click="showVehicle(scope.row.id)">查看车辆</el-button>
+          <el-button type="primary" link @click="showVehicle(scope.row.id)">Check vehicle</el-button>
         </template>
       </el-table-column>
     </el-table>
   </el-dialog>
 
-  <!-- 分配车辆弹窗 -->
-  <el-dialog title="分配车辆" style="padding:40px;" v-model="vehicleDialogVisible">
+  <!-- Allocation vehicle pop-up window -->
+  <el-dialog title="Allocation vehicle" style="padding:40px;" v-model="vehicleDialogVisible">
     <el-table style="margin:20px 0" :data="vehicleTableData">
-      <el-table-column label="编号" type="index" width="80" align="center"></el-table-column>
-      <el-table-column label="车辆品牌" prop="brand" width="200" align="center"></el-table-column>
-      <el-table-column label="车牌号" prop="license" width="200" align="center"></el-table-column>
-      <el-table-column label="车辆类型" prop="type" align="center"></el-table-column>
-      <el-table-column label="车辆状态" prop="status" align="center"></el-table-column>
-      <el-table-column label="操作" width="100" align="center">
+      <el-table-column label="Number" type="index" width="80" align="center"></el-table-column>
+      <el-table-column label="Vehicle brand" prop="brand" width="200" align="center"></el-table-column>
+      <el-table-column label="Vehicle number" prop="license" width="200" align="center"></el-table-column>
+      <el-table-column label="Vehicle type" prop="type" align="center"></el-table-column>
+      <el-table-column label="Vehicle status" prop="status" align="center"></el-table-column>
+      <el-table-column label="Operation" width="100" align="center">
         <template #default="scope">
-          <el-button type="primary" link @click="distribute(scope.row.id)">分配</el-button>
+          <el-button type="primary" link @click="distribute(scope.row.id)">Allocation</el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -107,9 +107,9 @@ import {onMounted, ref} from "vue";
 import axios from "axios";
 import qs from "qs";
 import {ElMessage} from "element-plus";
-//用于控制围栏车辆概览弹窗是否可见
+// Used to control whether the fenced vehicle overview pop-up is visible
 const dialogVisible = ref(false);
-//用于控制分配车辆弹窗是否可见
+// Used to control whether the vehicle allocation pop-up is visible.
 const vehicleDialogVisible = ref(false);
 
 const type = ref(50);
@@ -143,10 +143,10 @@ const searchGeofenceForm = ref({
 });
 const geoTableData = ref([]);
 
-//定义全局变量用来保存当前申请单的id,在分配车辆时使用
+// Define a global variable to store the ID of the current application form, which will be used when allocating vehicles
 var applicationId;
 const loadGeo=(id)=>{
-  //将当前申请单id赋值给全局变量
+  // Assign the current application ID to a global variable.
   applicationId = id;
 
   dialogVisible.value = true;
@@ -163,24 +163,24 @@ const resetSearchGeo = () => {
   searchGeofenceForm.value = {};
   loadGeo();
 }
-//对围栏状态显示的值进行转换
+// Convert the value displayed for the fence status
 const geofenceStatusFormatter = (row, column,cellValue,index) => {
   if(cellValue==1){
-    cellValue = '启用';
+    cellValue = 'Enable';
   }else if(cellValue==0){
-    cellValue = '禁用'
+    cellValue = 'Disable'
   }
   return cellValue;
 }
-//处理关闭围栏弹窗的方法
+// How to handle the "Close Fence" pop-up
 const handleClose = ()=>{
   searchGeofenceForm.value = {};
   dialogVisible.value = false;
 }
 
-//定义数组用来保存加载出来的围栏车辆数据
+// Define an array to store the loaded fence vehicle data
 const vehicleTableData = ref([]);
-//定义方法加载指定围栏上的车辆数据
+// Define a method to load vehicle data on a specified fence
 const showVehicle = (id)=>{
   vehicleDialogVisible.value = true;
   axios.get(BASE_URL+'/v1/vehicle/select?geofenceId='+id).then((response)=>{
@@ -192,39 +192,36 @@ const showVehicle = (id)=>{
   })
 }
 
-//分配车辆方法 给指定申请单分配指定的车辆 applicationId是全局变量,在loadGeo调用时已赋值
+//The vehicle allocation method assigns a specified vehicle to a specified application form. 
+  //`applicationId` is a global variable and was assigned a value when `loadGeo` is called.
 const distribute = (vehicleId)=>{
   axios.post(BASE_URL+'/v1/application/distribute/'+applicationId+'/'+vehicleId)
       .then((response)=>{
         if (response.data.code==2000){
-          dialogVisible.value = false;//关闭围栏列表弹窗
-          vehicleDialogVisible.value = false;//关闭车辆列表弹窗
-          loadApplication();//分配后重新加载申请单数据
-          ElMessage.success('车辆分配成功!');
+          dialogVisible.value = false; //Close the fence list pop-up
+          vehicleDialogVisible.value = false;// Close the vehicle list pop-up
+          loadApplication();//Reload application data after allocation
+          ElMessage.success('Vehicle allocation successful!');
         }else {
           ElMessage.error(response.data.msg);
         }
       })
 }
 
-//还车方法
-//row代表调用方法时传过来的整行申请单数据
-//row.id就是applicationId row.vehicleId就是当前申请单的车辆id
+//Return vehicle
 const back = (row)=>{
   console.log(row.id,typeof row.id);
   console.log(row.vehicleId,typeof row.vehicleId);
   axios.post(BASE_URL+'/v1/application/back/'+row.id+'/'+row.vehicleId)
       .then((response)=>{
         if (response.data.code==2000){
-          loadApplication();//还车后重新加载申请单数据
-          ElMessage.success('还车成功!');
+          loadApplication();//Reload the application data after returning the car.
+          ElMessage.success('Car returned successfully!');
         }else {
           ElMessage.error(response.data.msg);
         }
       })
 }
 </script>
-
 <style scoped>
-
 </style>
